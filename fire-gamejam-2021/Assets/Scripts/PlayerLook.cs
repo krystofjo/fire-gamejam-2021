c740e42f62dd77dd3a818b69ce6bab8fa948c6da
krystofjo <43005473+krystofjo@users.noreply.github.com>
2021-10-23 17:51:56 +0200

    Player look controls

--- fire-gamejam-2021/Assets/Scripts/PlayerLook.cs
+++ fire-gamejam-2021/Assets/Scripts/PlayerLook.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+
+    public float mouseSensitivity = 100f;
+    
+    public Transform playerBody;
+
+    float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,13 @@ public class PlayerLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float rightArrow = Input.GetAxis("Horizontal B") * mouseSensitivity * Time.deltaTime;
+        float upArrow = Input.GetAxis("Vertical B") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= upArrow;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * rightArrow);
     }
 }

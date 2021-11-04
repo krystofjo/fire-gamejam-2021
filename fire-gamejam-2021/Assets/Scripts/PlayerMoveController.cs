a94a954f02107d1af5571f18bfe60b689f63ccb4
Kryštof Josefus <josefus.krystof@gmail.com>
2021-11-04 12:02:59 +0100

    Arm placeholder, setting to joysticks

--- fire-gamejam-2021/Assets/Scripts/PlayerMoveController.cs
+++ fire-gamejam-2021/Assets/Scripts/PlayerMoveController.cs
@@ -21,7 +21,7 @@ public class PlayerMoveController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right *  x + transform.forward * z;
+        Vector3 move = transform.right *  x + transform.forward * -z;
 
         controller.Move(move * speed * Time.deltaTime);
     }

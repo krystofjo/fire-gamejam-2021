448a7e7473944c8ea5e621fdde9c5dcc8bf9358a
krystofjo <43005473+krystofjo@users.noreply.github.com>
2021-11-05 12:43:01 +0100

    grabbable object, grab controller

---
+++ fire-gamejam-2021/Assets/Scripts/GrabController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController : MonoBehaviour
+{   
+    public string input;
+    public GameObject grabbedObject;
+    public GameObject canGrab;
+
+    public Vector3 dropPosition = new Vector3(0f, -2.5f, 5f);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float buttonPressed = Input.GetAxis(input);
+        if(buttonPressed == 1) {
+                Debug.Log("Can Grab" + input);
+            canGrab.transform.parent = this.gameObject.transform;
+            if (grabbedObject == null) {
+                grabbedObject = canGrab;
+                grabbedObject.GetComponent<GrabbableObject>().isGrabbed = true; 
+                grabbedObject.transform.position = this.gameObject.transform.position;
+                grabbedObject.transform.rotation = Quaternion.Euler(grabbedObject.GetComponent<GrabbableObject>().grabbedRotation);
+            }
+        }
+        if(buttonPressed == 0) {
+            if(grabbedObject != null) {
+                grabbedObject.GetComponent<GrabbableObject>().isGrabbed = false; 
+                grabbedObject.transform.rotation = new Quaternion(0,0,0,0);
+                grabbedObject.transform.localPosition = dropPosition;
+                canGrab.transform.parent = GameObject.Find("Grabbables").transform;
+                grabbedObject = null;
+            }
+        }
+        
+    }
+}
---
+++ fire-gamejam-2021/Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour
+{
+    public bool isGrabbed = false;
+    public Vector3 grabbedPosition = new Vector3 (0, 0, 0);
+    public Vector3 grabbedRotation = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

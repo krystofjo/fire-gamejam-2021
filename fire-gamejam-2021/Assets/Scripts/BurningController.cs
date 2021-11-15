6cd1e45cf09e4be33fafba8f357724b32bc0404b
krystofjo <43005473+krystofjo@users.noreply.github.com>
2021-11-15 22:43:33 +0100

    fire regulations

---
+++ fire-gamejam-2021/Assets/Scripts/BurningController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurningController : MonoBehaviour
+{
+
+    public GameObject fire;
+    public Material burnMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Burnable") {
+            Debug.Log("Burnable"); 
+            fire.GetComponent<FireController>().fireTimeLeft += collider.gameObject.transform.parent.GetComponent<GrabbableObject>().stickPower;
+            collider.gameObject.transform.parent.tag = "Untagged";
+            collider.gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = burnMaterial;
+        }
+    }
+}
--- fire-gamejam-2021/Assets/Scripts/FireController.cs
+++ fire-gamejam-2021/Assets/Scripts/FireController.cs
@@ -9,6 +9,22 @@ public class FireController : MonoBehaviour
     public float fireInitialTime = 20;
     public float fireTimeLeft;
     public float burnOutSpeed = 1;
+
+    [Header("Limits")]
+    public float fireLimit1 = 20;
+    public float fireLimit2 = 50;
+    public float fireLimit3 = 120;
+    [Header("Intensity")]
+    public float intensity1 = 0.2f;
+    public float intensity2 = 0.5f;
+    public float intensity3 = 1f;
+    public float intensity4 = 2f;
+    [Header("Speed")]
+    public float speed1 = 1.5f;
+    public float speed2 = 1f;
+    public float speed3 = 0.5f;
+    public float speed4 = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +34,31 @@ public class FireController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        UpdateFirePower();
+
         if(fireTimeLeft > 0) {
             fireTimeLeft -= burnOutSpeed * Time.deltaTime;
         }
+    }
+
+    void UpdateFirePower() {
+            if(fireTimeLeft<fireLimit1) {
+                Debug.Log("intensity 1");
+                fireIntesity = intensity1;
+                burnOutSpeed = speed1;
+            } else if(fireTimeLeft>fireLimit1 && fireTimeLeft<fireLimit2) {
+                fireIntesity = intensity2;
+                burnOutSpeed = speed2;
+                Debug.Log("intensity 2");
+            }   else if(fireTimeLeft>fireLimit2 && fireTimeLeft<fireLimit3) {
+                fireIntesity = intensity3;
+                burnOutSpeed = speed3;
+                Debug.Log("intensity 3");
+            }   else if(fireTimeLeft>fireLimit3) {
+                fireIntesity = intensity4;
+                burnOutSpeed = speed4;
+                Debug.Log("intensity 4");
+            }
     }
 }

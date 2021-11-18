bd24c04eb64c835724b056e99ff42dcdedc93122
krystofjo <43005473+krystofjo@users.noreply.github.com>
2021-11-18 09:48:52 +0100

    polybrush settings

--- fire-gamejam-2021/Assets/Scripts/HomePointsController.cs
+++ fire-gamejam-2021/Assets/Scripts/HomePointsController.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class HomePointsController : MonoBehaviour
 {
     GameObject gameManager;
+    public PlayerMoveController playerMove;
     HomePointsCounter homePointsCounter;
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager");
         homePointsCounter = gameManager.GetComponent<HomePointsCounter>();
+        playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMoveController>();
     }
 
     void OnTriggerExit(Collider otherColider) 
     {
-        if(otherColider.tag == "HomePointBorder") {
-            Destroy(otherColider.gameObject);
+        if(otherColider.tag == "Player") {
             homePointsCounter.homePoints++;
+            playerMove.speed = playerMove.speed/2;
+            Destroy(gameObject);
         }
     }
 

0b18f2ecd4fea75cc6e6030fd9d876943a7bce00
krystofjo <43005473+krystofjo@users.noreply.github.com>
2021-11-19 07:25:19 +0100

    dialogue manager

---
+++ fire-gamejam-2021/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public DialoguePlayer dialoguePlayer;
+    public bool[] fireAlreadyPlayed;
+    public bool[] shelterAlreadyPlayed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
